cda713fb70daaef6a8f5a4230d1bca870713c529
Ekaterina Prianishnikova <li.liliana@yandex.ru>
2025-04-20 14:15:28 +0400

    add posts

--- src/routes/graphql/types/query.ts
+++ src/routes/graphql/types/query.ts
@@ -1,6 +1,9 @@
 import { GraphQLList, GraphQLNonNull, GraphQLObjectType } from 'graphql';
 import { MemberType, MemberTypeId } from './memberType.js';
 import { PrismaContext } from './prismaContext.js';
+import { PostType } from './postType.js';
+import { UUIDType } from './uuid.js';
+import { UUID } from 'crypto';
 
 export const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -16,6 +19,19 @@ export const RootQueryType = new GraphQLObjectType({
       args: { id: { type: new GraphQLNonNull(MemberTypeId) } },
       resolve: (_, args: { id: string }, context: PrismaContext) => {
         return context.prisma.memberType.findUnique({ where: { id: args.id } });
+      },
+    },
+    posts: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(PostType))),
+      resolve: (_, _args, context: PrismaContext) => {
+        return context.prisma.post.findMany();
+      },
+    },
+    post: {
+      type: new GraphQLNonNull(PostType),
+      args: { id: { type: new GraphQLNonNull(UUIDType) } },
+      resolve: (_, args: { id: UUID }, context: PrismaContext) => {
+        return context.prisma.post.findUnique({ where: { id: args.id } });
       },
     },
   },
