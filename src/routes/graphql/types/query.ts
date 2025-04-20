0fa7c00edb54c9ce29f254b81db0e3d913c42c77
Ekaterina Prianishnikova <li.liliana@yandex.ru>
2025-04-20 13:55:28 +0400

    add member types

--- src/routes/graphql/types/query.ts
+++ src/routes/graphql/types/query.ts
@@ -1,12 +1,21 @@
-import { GraphQLObjectType, GraphQLString } from 'graphql';
+import { GraphQLList, GraphQLNonNull, GraphQLObjectType } from 'graphql';
+import { MemberType, MemberTypeId } from './memberType.js';
+import { PrismaContext } from './prismaContext.js';
 
 export const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    hello: {
-      type: GraphQLString,
-      resolve() {
-        return 'world';
+    memberTypes: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(MemberType))),
+      resolve: (_, _args, context: PrismaContext) => {
+        return context.prisma.memberType.findMany();
+      },
+    },
+    memberType: {
+      type: new GraphQLNonNull(MemberType),
+      args: { id: { type: new GraphQLNonNull(MemberTypeId) } },
+      resolve: (_, args: { id: string }, context: PrismaContext) => {
+        return context.prisma.memberType.findUnique({ where: { id: args.id } });
       },
     },
   },
