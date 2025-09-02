5a5a62e0b24296a496510a9ff21841425e32a041
Ekaterina Prianishnikova <li.liliana@yandex.ru>
2025-09-02 20:31:18 +0400

    feat: add simple deps

--- src/routes/graphql/types/memberType.ts
+++ src/routes/graphql/types/memberType.ts
@@ -2,9 +2,12 @@ import {
   GraphQLEnumType,
   GraphQLFloat,
   GraphQLInt,
+  GraphQLList,
   GraphQLNonNull,
   GraphQLObjectType,
 } from 'graphql';
+import { ProfileType } from './profileType.js';
+import { PrismaContext } from './prismaContext.js';
 
 export const MemberTypeId = new GraphQLEnumType({
   name: 'MemberTypeId',
@@ -20,5 +23,11 @@ export const MemberType = new GraphQLObjectType({
     id: { type: new GraphQLNonNull(MemberTypeId) },
     discount: { type: new GraphQLNonNull(GraphQLFloat) },
     postsLimitPerMonth: { type: new GraphQLNonNull(GraphQLInt) },
+    profiles: {
+      type: new GraphQLList(ProfileType),
+      resolve: (parent: { id: string }, _args, context: PrismaContext) => {
+        return context.prisma.profile.findMany({ where: { memberTypeId: parent.id } });
+      },
+    },
   },
 });
--- src/routes/graphql/types/postType.ts
+++ src/routes/graphql/types/postType.ts
@@ -1,5 +1,7 @@
 import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
 import { UUIDType } from './uuid.js';
+import { UserType } from './userType.js';
+import { PrismaContext } from './prismaContext.js';
 
 export const PostType = new GraphQLObjectType({
   name: 'PostType',
@@ -7,5 +9,11 @@ export const PostType = new GraphQLObjectType({
     id: { type: new GraphQLNonNull(UUIDType) },
     title: { type: new GraphQLNonNull(GraphQLString) },
     content: { type: new GraphQLNonNull(GraphQLString) },
+    author: {
+      type: UserType,
+      resolve: (parent: { authorId: string }, _args, context: PrismaContext) => {
+        return context.prisma.user.findUnique({ where: { id: parent.authorId } });
+      },
+    },
   },
 });
