41ff5441b74f5c87c9001aab1cf2c9d53bb02962
Ekaterina Prianishnikova <li.liliana@yandex.ru>
2025-09-04 15:43:11 +0400

    feat: finish user type

--- src/routes/graphql/types/userType.ts
+++ src/routes/graphql/types/userType.ts
@@ -12,7 +12,7 @@ import { PostType } from './postType.js';
 
 export const UserType = new GraphQLObjectType({
   name: 'UserType',
-  fields: {
+  fields: () => ({
     id: { type: new GraphQLNonNull(UUIDType) },
     name: { type: new GraphQLNonNull(GraphQLString) },
     balance: { type: new GraphQLNonNull(GraphQLFloat) },
@@ -29,7 +29,7 @@ export const UserType = new GraphQLObjectType({
       },
     },
     userSubscribedTo: {
-      type: new GraphQLList(UserType),
+      type: new GraphQLNonNull(new GraphQLList(UserType)),
       resolve: async (parent: { id: string }, _args, context: PrismaContext) => {
         const deps = await context.prisma.subscribersOnAuthors.findMany({
           where: { subscriberId: parent.id },
@@ -38,5 +38,15 @@ export const UserType = new GraphQLObjectType({
         return context.prisma.user.findMany({ where: { id: { in: authorIds } } });
       },
     },
-  },
+    subscribedToUser: {
+      type: new GraphQLNonNull(new GraphQLList(UserType)),
+      resolve: async (parent: { id: string }, _args, context: PrismaContext) => {
+        const deps = await context.prisma.subscribersOnAuthors.findMany({
+          where: { authorId: parent.id },
+        });
+        const subscriberIds = deps.map((s) => s.subscriberId);
+        return context.prisma.user.findMany({ where: { id: { in: subscriberIds } } });
+      },
+    },
+  }),
 });
